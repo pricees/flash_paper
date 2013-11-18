cf198846e9578aa5112f3fa976abaf5f04d17cd9
Edward Price <ted.price@gmail.com>
2013-11-17 23:38:14 -0600

    Send data to firebase

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ flash_paper
 ===========
 
 Notes using Node + Firebase....its exploratory..
+
+npm install firebase
+
+
+FIREBASE_DB=[firebase db] node flashpaper.js [note]
+
---
+++ flashpaper.js
@@ -0,0 +1,21 @@
+/**
+ * This is an exploratory program.  I am finally learning Javascript.
+ *
+ * NOTE: Interestingly enough, Firebase writes locally, then writes the cloud
+ * when available.  
+ *
+ *
+ */
+var Firebase = require('firebase');
+
+if (process.argv.length > 2) {
+  var firebase_db = process.env.FIREBASE_DB;
+  var url_db      = 'https://' + firebase_db + '.firebaseio.com/'
+  var myRootRef   = new Firebase(url_db);
+  note            = process.argv[2];
+
+  myRootRef.child('note').push({ "note" : note, "category" : "stuff" }, 
+      function() { process.exit(); });
+} else {
+  console.log('FIREBASE_DB=[your_db] node flashpaper.js [note]');
+}
